8de5c1fb74a72b04641c107a10ab9aa9e9a31deb
Surbhi Sahu <surbhisahu3075@gmail.com>
2026-03-13 01:12:54 +0530

    [Surbhi]:Added Refactored Enum Test file

--- QuantityMeasurementapp/src/test/java/com/apps/quantitymeasurement/RefactoredEnumTestFile.java
+++ QuantityMeasurementapp/src/test/java/com/apps/quantitymeasurement/RefactoredEnumTestFile.java
@@ -54,7 +54,7 @@ public class RefactoredEnumTestFile {
 	// Quantity(2.54, CENTIMETERS).convertTo(INCHES)
 	@Test
 	public void givenCentimeters_whenConvertToInches_shouldReturn1Inch() {
-		Length cm = new Length(2.54, LengthUnit.CENTIMETER);
+		Length cm = new Length(2.54, LengthUnit.CENTIMETERS);
 		Length result = cm.convertTo(LengthUnit.INCHES);
 
 		assertEquals(1.0, result.getValue(), EPSILON);
