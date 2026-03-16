08eca51397e919081518131809769d3ffeca36a8
Surbhi Sahu <surbhisahu3075@gmail.com>
2026-03-16 23:37:09 +0530

    [Surbhi]:Added FeetEquality file

--- QuantityMeasurementapp/src/test/java/com/apps/quantitymeasurement/FeetEquality.java
+++ QuantityMeasurementapp/src/test/java/com/apps/quantitymeasurement/FeetEquality.java
@@ -1,6 +1,7 @@
 package com.apps.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FeetEquality {
 
@@ -8,36 +9,31 @@ class FeetEquality {
     void testEquality_SameValue() {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
-
-        assertTrue(f1.equals(f2), "1.0 ft should equal 1.0 ft");
+        assertTrue(f1.equals(f2));
     }
 
     @Test
     void testEquality_DifferentValue() {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(2.0);
-
-        assertFalse(f1.equals(f2), "1.0 ft should not equal 2.0 ft");
+        assertFalse(f1.equals(f2));
     }
 
     @Test
     void testEquality_NullComparison() {
         Feet f1 = new Feet(1.0);
-
-        assertFalse(f1.equals(null), "Feet should not equal null");
+        assertFalse(f1.equals(null));
     }
 
     @Test
     void testEquality_SameReference() {
         Feet f1 = new Feet(1.0);
-
-        assertTrue(f1.equals(f1), "Object should equal itself");
+        assertTrue(f1.equals(f1));
     }
 
     @Test
     void testEquality_DifferentType() {
         Feet f1 = new Feet(1.0);
-
-        assertFalse(f1.equals("1.0"), "Feet should not equal String");
+        assertFalse(f1.equals("1.0"));
     }
 }
