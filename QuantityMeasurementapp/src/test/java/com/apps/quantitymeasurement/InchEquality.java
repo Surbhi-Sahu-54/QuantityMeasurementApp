5a1a241dbc8d8ea4049159c32a43600fd291e7a6
Surbhi Sahu <surbhisahu3075@gmail.com>
2026-03-16 23:38:34 +0530

    [Surbhi]:Added Inch Quantity Test file

--- QuantityMeasurementapp/src/test/java/com/apps/quantitymeasurement/InchEquality.java
+++ QuantityMeasurementapp/src/test/java/com/apps/quantitymeasurement/InchEquality.java
@@ -2,6 +2,8 @@ package com.apps.quantitymeasurement;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class InchEquality {
 
@@ -9,36 +11,31 @@ class InchEquality {
     void testEquality_SameValue() {
         Inches i1 = new Inches(1.0);
         Inches i2 = new Inches(1.0);
-
-        assertTrue(i1.equals(i2), "1.0 inch should equal 1.0 inch");
+        assertTrue(i1.equals(i2));
     }
 
     @Test
     void testEquality_DifferentValue() {
         Inches i1 = new Inches(1.0);
         Inches i2 = new Inches(2.0);
-
-        assertFalse(i1.equals(i2), "1.0 inch should not equal 2.0 inch");
+        assertFalse(i1.equals(i2));
     }
 
     @Test
     void testEquality_NullComparison() {
         Inches i1 = new Inches(1.0);
-
-        assertFalse(i1.equals(null), "Inches should not equal null");
+        assertFalse(i1.equals(null));
     }
 
     @Test
     void testEquality_SameReference() {
         Inches i1 = new Inches(1.0);
-
-        assertTrue(i1.equals(i1), "Object should equal itself");
+        assertTrue(i1.equals(i1));
     }
 
     @Test
     void testEquality_DifferentType() {
         Inches i1 = new Inches(1.0);
-
-        assertFalse(i1.equals(1.0), "Inches should not equal different type");
+        assertFalse(i1.equals(1.0));
     }
 }
