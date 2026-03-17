6204a6855e21f7ff7819c22bd577eeea74d6f6a3
Surbhi Sahu <surbhisahu3075@gmail.com>
2026-03-17 13:07:34 +0530

    [Surbhi]:Added test-data.sql file

---
+++ QuantityMeasurementapp/src/test/resources/test-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO quantity_measurement_entity
+(this_value, this_unit, this_measurement_type, that_value, that_unit, that_measurement_type, operation, result_string)
+VALUES
+(1.0, 'FEET', 'LengthUnit', 12.0, 'INCHES', 'LengthUnit', 'COMPARE', 'Equal');
