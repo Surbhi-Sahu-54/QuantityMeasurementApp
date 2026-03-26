3ef5af289da7b1c0909044a1913d055c87ecf19a
Surbhi Sahu <surbhisahu3075@gmail.com>
2026-03-26 11:05:38 +0530

    [Surbhi]:Added SecurityConfig file

---
+++ src/main/java/com/app/quantitymeasurement/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.app.quantitymeasurement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/h2-console/**"
+                ).permitAll()
+                .anyRequest().permitAll()
+            );
+
+        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+        return http.build();
+    }
+}
