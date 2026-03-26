8a6cb074ae11d143079944b2407ad61d740186b9
Surbhi Sahu <surbhisahu3075@gmail.com>
2026-03-26 11:18:51 +0530

    [Surbhi]:Added Quantity Model file

---
+++ src/main/java/com/app/quantitymeasurement/model/QuantityModel.java
@@ -0,0 +1,46 @@
+package com.app.quantitymeasurement.model;
+import com.app.quantitymeasurement.measurement.IMeasureable;
+import com.app.quantitymeasurement.measurement.Quantity;
+
+import lombok.Getter;
+
+/*
+ * =========================================================
+ * QuantityModel
+ * =========================================================
+ *
+ * UC15 – Model Layer
+ *
+ * Purpose:
+ * Internal representation of a quantity used inside
+ * the Service layer.
+ *
+ * This separates internal processing from external DTOs.
+ *
+ * Model -> used for computations
+ * DTO   -> used for communication
+ */
+
+@Getter
+public class QuantityModel<U extends IMeasureable> {
+
+    private Quantity<U> quantity;
+
+    public QuantityModel(double value, U unit) {
+        this.quantity = new Quantity<>(value, unit);
+    }
+
+
+    public double getValue() {
+        return quantity.getValue();
+    }
+
+    public U getUnit() {
+        return quantity.getUnit();
+    }
+
+    @Override
+    public String toString() {
+        return quantity.toString();
+    }
+}
